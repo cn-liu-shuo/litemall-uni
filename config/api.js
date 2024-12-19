22606046bcec3cba1a63457bfb7c9bc4156f9f92
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-19 16:34:59 +0800

    新增：封装获取首页数据接口

---
+++ config/api.js
@@ -0,0 +1,11 @@
+const base_url = 'http://admin.bsmall.byesame.com/wx'
+
+// 获取首页数据
+export function homeIndex() {
+  return new Promise((resolve,reject) => {
+    uni.request({
+      url: base_url + '/home/index',
+      method: 'GET'
+    })
+  })
+}
