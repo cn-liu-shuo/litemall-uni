78ffbc2b01e6c4d2e93da3e8e7496f47515e8cbd
cn_liushuo <cn_liushuo@foxmail.com>
2024-12-20 18:31:18 +0800

    新增：完成登录页头像渲染

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,6 +1,10 @@
 <template>
-  <view>
-    登录
+  <view class="login">
+    <view class="store_header">
+      <view class="store_avatar">
+        <image src="../../static/image/avatar_default.89845f4e.png" mode=""></image>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,15 +12,27 @@
   export default {
     data() {
       return {
-        
+
       }
     },
     methods: {
-      
+
     }
   }
 </script>
 
-<style>
+<style scoped lang="scss">
+  .store_header {
+    text-align: center;
+    padding: 60rpx 0;
 
+    .store_avatar {
+      text-align: center;
+      image {
+        width: 110rpx;
+        height: 110rpx;
+        border-radius: 50%;
+      }
+    }
+  }
 </style>
